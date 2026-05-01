20bff34d8a004690805f6c9eda8271081e2cb063
Carlos Gonzalez <carlos.gabriel.g.d@gmail.com>
2026-04-30 17:30:28 -0700

    update_1.1

---
+++ mapa_mexico.js
@@ -0,0 +1,201 @@
+const santos_de_mexico = {
+    MXSON:{
+        Estado: "Sonora",
+        Contiene: [ ]
+    },
+    MXBCN:{
+        Estado: "Baja California ",
+        Contiene: [ ]
+    },
+    MXCHH:{
+        Estado: "Chihuahua",
+        Contiene: [
+            {Nombre:"San Pedro de Jesús Maldonado Lucero",
+            Enlace: "https://es.wikipedia.org/wiki/Pedro_de_Jes%C3%BAs_Maldonado"}
+        ]
+    },
+    MXCOA:{
+        Estado: "Coahuila",
+        Contiene: [ ]
+    },
+    MXTAM:{
+        Estado: "Tamaulipas",
+        Contiene: [ ]
+    },
+    MXNLE:{
+        Estado: "Nuevo Leon",
+        Contiene:[ ]
+    },
+    MXROO:{
+        Estado: "Quintana Roo",
+        Contiene: [ ]
+    },
+    MXCAM:{
+        Estado: "Campeche",
+        Contiene: [ ]
+    },
+    MXTAB:{
+        Estado: "Tabasco",
+        Contiene: [ ]
+    },
+    MXCHP:{
+        Estado: "Chiapas",
+        Contiene: [ ]
+    },
+    MXCOL:{
+        Estado: "Colima",
+        Contiene: [ ]
+    },
+    MXNAY:{
+        Estado: "Nayarit",
+        Contiene: [ ]
+    },
+    MXBCS:{
+        Estado: "Baja California Sur",
+        Contiene: [ ]
+    },
+    MXSIN:{
+        Estado: "Sinaloa",
+        Contiene: [ ]
+    },
+    MXYUC:{
+        Estado: "Yucatán",
+        Contiene: [ ]
+    },
+    MXVER:{
+        Estado: "Veracruz",
+        Contiene: [ ]
+    },
+    MXJAL:{
+        Estado: "Jalisco",
+        Contiene: [{ Nombre: "San Cristóbal Magallanes Jara", Enlace: "https://en.wikipedia.org/wiki/Crist%C3%B3bal_Magallanes"},
+            {Nombre:"San Toribio Romo González", Enlace: "https://es.wikipedia.org/wiki/Toribio_Romo_Gonz%C3%A1lez"}, {Nombre:"San José María Robles Hurtado", Enlace: "https://es.wikipedia.org/wiki/Jos%C3%A9_Mar%C3%ADa_Robles_Hurtado"}, {Nombre:"San Sabás Reyes Salazar,", Enlace: "https://en.wikipedia.org/wiki/Sab%C3%A1s_Reyes_Salazar"}, {Nombre:"San Rodrigo Aguilar Alemán", Enlace: "https://es.wikipedia.org/wiki/Rodrigo_Aguilar_Alem%C3%A1n"},
+            {Nombre:"San Román Adame Rosales", Enlace: "https://es.wikipedia.org/wiki/Rom%C3%A1n_Adame_Rosales"}, {Nombre:"San Justino Orona Madrigal", Enlace: "https://es.wikipedia.org/wiki/Justino_Orona_Madrigal"},
+            {Nombre:"San Pedro Esqueda Ramírez", Enlace: "https://es.wikipedia.org/wiki/Pedro_Esqueda_Ram%C3%ADrez"}, {Nombre:"San David Galván Bermudes", Enlace: "https://en.wikipedia.org/wiki/David_Galv%C3%A1n_Berm%C3%BAdez"}
+        ]
+    },
+    MXMIC:{
+        Estado: "Michoacán",
+        Contiene: [{ Nombre: "San José Sanchez del Rio", Enlace: "https://en.wikipedia.org/wiki/Jos%C3%A9_S%C3%A1nchez_del_R%C3%ADo"}, {
+            Nombre:"San Rafael Guízar y Valencia", Enlace: "https://es.wikipedia.org/wiki/Rafael_Gu%C3%ADzar_y_Valencia"
+        }, {
+            Nombre: "San Bernabé de Jesús Méndez Montoya ", Enlace: "https://es.wikipedia.org/wiki/Bernab%C3%A9_de_Jes%C3%BAs_M%C3%A9ndez_Montoya"
+        }
+        ]
+    },
+    MXGRO:{
+        Estado: "Guerrero",
+        Contiene: [{ Nombre: "San David Urive Velasco", Enlace: "https://es.wikipedia.org/wiki/David_Uribe_Velasco"
+    }]},
+    MXOAX:{
+        Estado: "Oaxaca",
+        Contiene: [{ Nombre: "San Juan Bautista de los Angeles", Enlace: "https://en.wikipedia.org/wiki/Martyrs_of_Cajonos"}, {
+            Nombre:"San Jacinto de los Angeles", Enlace:"https://en.wikipedia.org/wiki/Martyrs_of_Cajonos"
+        }
+        ]
+    },
+    MXMEX:{
+        Estado: "Estado de Mexico",
+        Contiene: [{ Nombre: "San Jose Maria de Yermo y Porres", Enlace: "https://es.wikipedia.org/wiki/Jos%C3%A9_Mar%C3%ADa_de_Yermo_y_Parres"
+    }]
+    },
+    MXPUE:{
+        Estado: "Puebla",
+        Contiene: [ ]
+    },
+    MXMOR:{
+        Estado: "Morelos",
+        Contiene: [ ]
+    },
+    MXQUE:{
+        Estado: "Querétaro",
+        Contiene: [ ]
+    },
+    MXHID:{
+        Estado: "Hidalgo",
+        Contiene: [ ]
+    },
+    MXGUA:{
+        Estado: "Guanajuato",
+        Contiene: [ ]
+    },
+    MXSLP:{
+        Estado: "San Luis Potosi",
+        Contiene: [ ]
+    },
+    MXZAC:{
+        Estado: "Zacatecas",
+        Contiene: [{ Nombre: "San Manuel Morales", Enlace: "https://en.wikipedia.org/wiki/Manuel_Morales_(martyr)"
+        }, { Nombre: "San José Isabel Flores Varela", Enlace: "https://es.wikipedia.org/wiki/Jos%C3%A9_Isabel_Flores_Varela"}]
+    },
+    MXAGU:{
+        Estado: "Aguascalientes",
+        Contiene: [ ]
+    },
+    MXDUR:{
+        Estado: "Durango",
+        Contiene: [{ Nombre: "San Jose Salvador Lara Puente", Enlace: "https://es.wikipedia.org/wiki/Salvador_Lara_Puente"
+        }]
+    },
+    MXTLA:{
+        Estado: "Tlaxcala",
+        Contiene: [{ Nombre: "San Antonio", Enlace: "https://es.wikipedia.org/wiki/Ni%C3%B1os_M%C3%A1rtires_de_Tlaxcala"
+        }, { Nombre: "San Juan", Enlace: "https://es.wikipedia.org/wiki/Ni%C3%B1os_M%C3%A1rtires_de_Tlaxcala"
+        }, { Nombre: "San Cristobal", Enlace: "https://es.wikipedia.org/wiki/Ni%C3%B1os_M%C3%A1rtires_de_Tlaxcala"
+        }]
+    },
+    MXCMX:{
+        Estado: "Ciudad de Mexico",
+        Contiene: [{ Nombre: "San Felipe de Jesus", Enlace: "https://en.wikipedia.org/wiki/Philip_of_Jesus"
+        }]
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const infoDiv = document.getElementById("info");
+    const estados = document.querySelectorAll("#label_points circle");
+
+    console.log("Estados encontrados:", estados.length); 
+
+    estados.forEach(estado => {
+        estado.addEventListener("click", () => {
+            const estadoId = estado.id;
+            const informati = santos_de_mexico[estadoId];
+
+            console.log("Presiono", estadoId, informati); 
+
+            if (!informati) return;
+
+            if (informati.Contiene.length === 0) {
+                infoDiv.innerHTML = `
+                    <h2>${informati.Estado}</h2>
+                    <p>Ningun Santo nacio aqui.</p>
+                `;
+                return;
+            }
+
+            let html = "<ul>";
+
+            informati.Contiene.forEach(santo => {
+                html += `
+                    <li>
+                        ${
+                            santo.Enlace
+                            ? `<a href="${santo.Enlace}" target="_blank">${santo.Nombre}</a>`
+                            : santo.Nombre
+                        }
+                    </li>
+                `;
+            });
+
+            html += "</ul>";
+
+            infoDiv.innerHTML = `
+                <h2>${informati.Estado}</h2>
+                ${html}
+            `;
+        });
+    });
+
+});
